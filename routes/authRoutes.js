1d2b08a364ca484d40b719037092598e2bda726a
Colin Ladley <colinladley@hotmail.com>
2019-01-17 16:49:13 +0000

    Initial react stuff

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,21 +1,31 @@
-const passport = require('passport');
+const passport = require("passport");
 
-module.exports = (app) => {
+module.exports = app => {
   app.get(
     "/auth/google",
     passport.authenticate("google", {
       scope: ["profile", "email"]
     })
   );
-  
-  app.get("/auth/google/callback", passport.authenticate("google"));
 
-  app.get('/api/current_user', (req, res) => {
+  app.get("/auth/test", (req, res) => {
+    res.send("Yoo test");
+  });
+
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google"),
+    (req, res) => {
+      res.redirect("/surveys");
+    }
+  );
+
+  app.get("/api/current_user", (req, res) => {
     res.send(req.user);
   });
 
-  app.get('/api/logout', (req, res) => {
+  app.get("/api/logout", (req, res) => {
     req.logout();
-    res.send(req.user);
+    res.redirect("/");
   });
 };
